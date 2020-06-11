b69ae1a815168e0ac406a81b80c3ec8d6d2074a7
Chris Knight <cknight1234@gmail.com>
2020-06-11 23:00:21 +0100

    feat: add property-based redaction

---
+++ obfuscators/propertyRedaction.ts
@@ -0,0 +1,76 @@
+import {
+  Obfuscator,
+  Stream,
+  LogRecord,
+} from "../types.ts";
+import { Level } from "../levels.ts";
+
+export class PropertyRedaction implements Obfuscator {
+  #redactionKey: string;
+
+  constructor(propertyToRedact: string) {
+    this.#redactionKey = propertyToRedact;
+  }
+
+  obfuscate(stream: Stream, logRecord: LogRecord): LogRecord {
+    return new ObfuscatedPropertyLogRecord(logRecord, this.#redactionKey);
+  }
+}
+
+class ObfuscatedPropertyLogRecord implements LogRecord {
+  readonly msg: unknown;
+  #metadata: unknown[];
+  #dateTime: Date;
+  readonly level: Level;
+  #logRecord: LogRecord;
+
+  constructor(logRecord: LogRecord, property: string) {
+    if (typeof logRecord.msg === "object") {
+      this.msg = JSON.parse(JSON.stringify(logRecord.msg));
+      this.redact(this.msg, property);
+    } else {
+      this.msg = logRecord.msg;
+    }
+
+    if (property === "metadata") {
+      this.#metadata = ["[Redacted]"];
+    } else {
+      this.#metadata = JSON.parse(JSON.stringify(logRecord.metadata));
+      for (let i = 0; i < this.#metadata.length; i++) {
+        if (typeof (this.#metadata[i]) === "object") {
+          this.redact(this.#metadata[i], property);
+        }
+      }
+    }
+
+    this.level = logRecord.level;
+    this.#dateTime = logRecord.dateTime;
+    this.#logRecord = logRecord;
+  }
+
+  redact(obj: unknown, property: string): void {
+    if (obj && typeof obj === "object") {
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          const castObj = (obj as { [key: string]: any });
+          if (key === property) {
+            castObj[key] = "[Redacted]";
+          } else if (typeof castObj[key] === "object") {
+            this.redact(castObj[key], property);
+          }
+        }
+      }
+    }
+  }
+
+  get dateTime(): Date {
+    return new Date(this.#dateTime.getTime());
+  }
+
+  get metadata(): unknown[] {
+    return [...this.#metadata];
+  }
+  get logRecord(): LogRecord {
+    return this.#logRecord;
+  }
+}
---
+++ obfuscators/propertyRedaction_test.ts
@@ -0,0 +1,140 @@
+import {
+  test,
+  assertEquals,
+  assertMatch,
+  assert,
+} from "../test_deps.ts";
+import { Level } from "../levels.ts";
+import { PropertyRedaction } from "./propertyRedaction.ts";
+import { LogRecord } from "../types.ts";
+
+const noopStream = { handle(lr: LogRecord): void {} };
+const REDACTED = "[Redacted]";
+
+test({
+  name: "Non matching redaction results in same object values",
+  fn() {
+    const lr = {
+      msg: "Log Message",
+      metadata: [{ a: true, b: "hello" }],
+      dateTime: new Date("2020-06-17T03:24:00"),
+      level: Level.DEBUG,
+    };
+    const newLr = new PropertyRedaction("x").obfuscate(noopStream, lr);
+    assertEquals(newLr.msg, lr.msg);
+    assertEquals(newLr.metadata, lr.metadata);
+    assertEquals(newLr.dateTime, lr.dateTime);
+    assertEquals(newLr.level, lr.level);
+  },
+});
+
+test({
+  name: "Test shallow msg object redaction",
+  fn() {
+    const lr = {
+      msg: { a: 6, b: "hello" },
+      metadata: ["The metadata"],
+      dateTime: new Date("2020-06-17T03:24:00"),
+      level: Level.DEBUG,
+    };
+    const newLrA = new PropertyRedaction("a").obfuscate(noopStream, lr);
+    const newLrB = new PropertyRedaction("b").obfuscate(noopStream, lr);
+    assertEquals(newLrA.msg, { a: REDACTED, b: "hello" });
+    assertEquals(newLrB.msg, { a: 6, b: REDACTED });
+    assertEquals(lr.msg, { a: 6, b: "hello" });
+  },
+});
+
+test({
+  name: "Test deep msg object redaction",
+  fn() {
+    const lr = {
+      msg: { a: { b: { c: { d: "hello" } } }, e: true },
+      metadata: ["The metadata"],
+      dateTime: new Date("2020-06-17T03:24:00"),
+      level: Level.DEBUG,
+    };
+    const newLrA = new PropertyRedaction("a").obfuscate(noopStream, lr);
+    const newLrB = new PropertyRedaction("b").obfuscate(noopStream, lr);
+    const newLrC = new PropertyRedaction("c").obfuscate(noopStream, lr);
+    const newLrD = new PropertyRedaction("d").obfuscate(noopStream, lr);
+    assertEquals(newLrA.msg, { a: REDACTED, e: true });
+    assertEquals(newLrB.msg, { a: { b: REDACTED }, e: true });
+    assertEquals(newLrC.msg, { a: { b: { c: REDACTED } }, e: true });
+    assertEquals(newLrD.msg, { a: { b: { c: { d: REDACTED } } }, e: true });
+    assertEquals(lr.msg, { a: { b: { c: { d: "hello" } } }, e: true });
+  },
+});
+
+test({
+  name: "Test shallow metadata object redaction",
+  fn() {
+    const lr = {
+      msg: "hello",
+      metadata: [{ a: 6, b: true }, "The metadata", { c: "hello", d: "world" }],
+      dateTime: new Date("2020-06-17T03:24:00"),
+      level: Level.DEBUG,
+    };
+    const newLrA = new PropertyRedaction("a").obfuscate(noopStream, lr);
+    const newLrB = new PropertyRedaction("b").obfuscate(noopStream, lr);
+    const newLrC = new PropertyRedaction("c").obfuscate(noopStream, lr);
+    const newLrD = new PropertyRedaction("d").obfuscate(noopStream, lr);
+    assertEquals(
+      newLrA.metadata,
+      [{ a: REDACTED, b: true }, "The metadata", { c: "hello", d: "world" }],
+    );
+    assertEquals(
+      newLrB.metadata,
+      [{ a: 6, b: REDACTED }, "The metadata", { c: "hello", d: "world" }],
+    );
+    assertEquals(
+      newLrC.metadata,
+      [{ a: 6, b: true }, "The metadata", { c: REDACTED, d: "world" }],
+    );
+    assertEquals(
+      newLrD.metadata,
+      [{ a: 6, b: true }, "The metadata", { c: "hello", d: REDACTED }],
+    );
+  },
+});
+
+test({
+  name: "Test deep msg object redaction",
+  fn() {
+    const lr = {
+      msg: "hello",
+      metadata: [
+        { a: { b: { c: { d: "hello" } } }, e: true },
+        { a: { b: { c: { d: "hello" } } }, e: true },
+      ],
+      dateTime: new Date("2020-06-17T03:24:00"),
+      level: Level.DEBUG,
+    };
+    const newLrA = new PropertyRedaction("a").obfuscate(noopStream, lr);
+    const newLrB = new PropertyRedaction("b").obfuscate(noopStream, lr);
+    const newLrC = new PropertyRedaction("c").obfuscate(noopStream, lr);
+    const newLrD = new PropertyRedaction("d").obfuscate(noopStream, lr);
+    assertEquals(
+      newLrA.metadata,
+      [{ a: REDACTED, e: true }, { a: REDACTED, e: true }],
+    );
+    assertEquals(
+      newLrB.metadata,
+      [{ a: { b: REDACTED }, e: true }, { a: { b: REDACTED }, e: true }],
+    );
+    assertEquals(
+      newLrC.metadata,
+      [
+        { a: { b: { c: REDACTED } }, e: true },
+        { a: { b: { c: REDACTED } }, e: true },
+      ],
+    );
+    assertEquals(
+      newLrD.metadata,
+      [
+        { a: { b: { c: { d: REDACTED } } }, e: true },
+        { a: { b: { c: { d: REDACTED } } }, e: true },
+      ],
+    );
+  },
+});
