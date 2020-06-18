72ea139348cc660357c4ed1d1d60395929f8281b
Chris Knight <cknight1234@gmail.com>
2020-06-18 22:16:35 +0100

    test: add stream tests

---
+++ streams/baseStream_test.ts
@@ -0,0 +1,115 @@
+import {
+  test,
+  assertEquals,
+  assert,
+} from "../test_deps.ts";
+import { BaseStream } from "./baseStream.ts";
+import { LogRecord, Formatter } from "../types.ts";
+import { Level } from "../levels.ts";
+
+class MsgPassThrough implements Formatter<string> {
+  format(lr: LogRecord): string {
+    return lr.msg === "format" ? "formatted!" : String(lr.msg);
+  }
+}
+
+class AlternativeMsgPassThrough implements Formatter<string> {
+  format(lr: LogRecord): string {
+    return "world";
+  }
+}
+
+class TestStream extends BaseStream {
+  logs: string[] = [];
+
+  constructor() {
+    super(new MsgPassThrough());
+  }
+
+  log(msg: string): void {
+    this.logs.push(msg);
+  }
+}
+
+function newBaseStream(): TestStream {
+  return new TestStream();
+}
+
+function logRec(msg: string, level: Level): LogRecord {
+  return {
+    msg: msg,
+    dateTime: new Date(),
+    metadata: [],
+    level: level,
+  };
+}
+
+test({
+  name: "Default base stream will log debug messages",
+  fn() {
+    const baseStream = newBaseStream();
+    baseStream.handle(logRec("hello", Level.DEBUG));
+    assertEquals(baseStream.logs[0], "hello");
+  },
+});
+
+test({
+  name: "Stream with higher min log level than log record won't log message",
+  fn() {
+    const baseStream = newBaseStream().minLogLevel(Level.INFO);
+    baseStream.handle(logRec("hello", Level.DEBUG));
+    assertEquals(baseStream.logs.length, 0);
+  },
+});
+
+test({
+  name: "Log messages are formatted by the default format function",
+  fn() {
+    const baseStream = newBaseStream();
+    baseStream.handle(logRec("format", Level.DEBUG));
+    assertEquals(baseStream.logs[0], "formatted!");
+  },
+});
+
+test({
+  name: "Alternative formatter function is used if set",
+  fn() {
+    const baseStream = newBaseStream().withFormat(
+      new AlternativeMsgPassThrough(),
+    );
+    baseStream.handle(logRec("hello", Level.DEBUG));
+    assertEquals(baseStream.logs[0], "world");
+  },
+});
+
+test({
+  name: "Output header defaults to true, but can be turned off",
+  fn() {
+    const baseStream = newBaseStream(); // default = true
+    assert(baseStream.outputHeader);
+    baseStream.withLogHeader(false); // explicitly set to false
+    assert(!baseStream.outputHeader);
+    baseStream.withLogHeader(); // implicitly set to true
+    assert(baseStream.outputHeader);
+    baseStream.withLogHeader(false); // explicitly set to false
+    assert(!baseStream.outputHeader);
+    baseStream.withLogHeader(true); // explicitly set to true
+    assert(baseStream.outputHeader);
+  },
+});
+
+test({
+  name: "Output footer defaults to true, but can be turned off",
+  fn() {
+    const baseStream = newBaseStream(); // default = true
+    assert(baseStream.outputFooter);
+    baseStream.withLogFooter(false); // explicitly set to false
+    assert(!baseStream.outputFooter);
+    baseStream.withLogFooter(); // implicitly set to true
+    assert(baseStream.outputFooter);
+    baseStream.withLogFooter(false); // explicitly set to false
+    assert(!baseStream.outputFooter);
+    baseStream.withLogFooter(true); // explicitly set to true
+    assert(baseStream.outputFooter);
+  },
+});
---
+++ streams/consoleStream_test.ts
@@ -0,0 +1,78 @@
+import {
+  test,
+  assertEquals,
+  assert,
+} from "../test_deps.ts";
+import { ConsoleStream } from "./consoleStream.ts";
+import { Level } from "../levels.ts";
+
+test({
+  name: "Default ConsoleStream logs message to console",
+  fn() {
+    const savedMsgs: unknown[] = [];
+    const savedLogFn = console.log;
+    try {
+      console.log = (...args: unknown[]) => {
+        savedMsgs.push(...args);
+      };
+      new ConsoleStream().log("hello");
+      assertEquals(savedMsgs[0], "hello");
+    } finally {
+      console.log = savedLogFn;
+    }
+  },
+});
+
+test({
+  name: "Default ConsoleStream logs color log record to console",
+  fn() {
+    const savedMsgs: unknown[] = [];
+    const savedLogFn = console.log;
+    try {
+      console.log = (...args: unknown[]) => {
+        savedMsgs.push(...args);
+      };
+      new ConsoleStream().handle(
+        {
+          msg: "hello",
+          level: Level.DEBUG,
+          metadata: [],
+          dateTime: new Date(),
+        },
+      );
+      assert(
+        (savedMsgs[0] as string).match(
+          /\d{4}[-]\d{2}[-]\d{2}[T]\d{2}[:]\d{2}[:]\d{2}[.]\d{3}[Z] DEBUG    hello/,
+        ),
+      );
+    } finally {
+      console.log = savedLogFn;
+    }
+  },
+});
+
+test({
+  name:
+    "If logHeader and logFooter are not set to output, then nothing is output",
+  fn() {
+    const savedMsgs: unknown[] = [];
+    const savedLogFn = console.log;
+    try {
+      console.log = (...args: unknown[]) => {
+        savedMsgs.push(...args);
+      };
+      const cs = new ConsoleStream().withLogHeader(false).withLogFooter(false);
+      const lm = {
+        hostname: "",
+        sessionStarted: new Date(),
+        minLogLevel: Level.DEBUG,
+        minLogLevelFrom: "somewhere",
+      };
+      cs.logHeader(lm);
+      cs.logFooter(lm);
+      assert(savedMsgs.length === 0);
+    } finally {
+      console.log = savedLogFn;
+    }
+  },
+});
