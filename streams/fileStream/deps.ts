fc9c51938ca77ff207fb9c2e1e8b7967ad0c6394
Chris Knight <cknight1234@gmail.com>
2020-08-21 23:01:31 +0100

    chore: update to v0.66 of std deps

--- streams/fileStream/deps.ts
+++ streams/fileStream/deps.ts
@@ -1,9 +1,9 @@
 export {
   basename as posixBasename,
   dirname as posixDirname,
-} from "https://deno.land/std@v0.61.0/path/posix.ts";
+} from "https://deno.land/std@0.66.0/path/posix.ts";
 export {
   basename as win32Basename,
   dirname as win32Dirname,
-} from "https://deno.land/std@v0.61.0/path/win32.ts";
-export { BufWriterSync } from "https://deno.land/std@v0.61.0/io/bufio.ts";
+} from "https://deno.land/std@0.66.0/path/win32.ts";
+export { BufWriterSync } from "https://deno.land/std@0.66.0/io/bufio.ts";
