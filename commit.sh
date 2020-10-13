cf48473d10011592f482754124b4f64ba205bcbb
Chris Knight <cknight1234@gmail.com>
2020-10-13 15:21:49 +0100

    chore: Update order of commits

---
+++ commit.sh
@@ -0,0 +1,26 @@
+#
+# commit.sh - Run this script prior to commiting any changes to git to ensure
+# that all code is properly updated, formatted, linted and tested.
+#
+echo '*** Removing erroneous test output ***'
+rm -f log*.txt*
+rm -f *.log*
+
+echo '*** Adding to git'
+git add .
+
+echo '*** Updating dependencies'
+eggs update --file ./deps.ts
+eggs update --file ./streams/fileStream/deps.ts
+
+echo '*** Formatting code'
+deno fmt
+
+echo '*** Linting code ***'
+deno lint --unstable
+
+echo '*** Testing code'
+deno test -A
+
+echo '*** Checking git status'
+git status
