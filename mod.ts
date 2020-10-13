158c9b00d04fe843166b4ea6bb6321e750b10c9e
Chris Knight <cknight1234@gmail.com>
2020-10-13 15:09:37 +0100

    refactor: make exports explicit

--- mod.ts
+++ mod.ts
@@ -1,15 +1,35 @@
 // Copyright 2020 the optic authors. All rights reserved. MIT license.
 export { RegExFilter } from "./filters/regExFilter.ts";
 export { SubStringFilter } from "./filters/subStringFilter.ts";
-export type { ColorRule } from "./formatters/color.ts";
+export { ColorRule } from "./formatters/color.ts";
 export { getColorForLevel } from "./formatters/color.ts";
 export { JsonFormatter } from "./formatters/json.ts";
 export { TokenReplacer } from "./formatters/tokenReplacer.ts";
 export { SimpleDateTimeFormatter } from "./formatters/simpleDateTimeFormatter.ts";
-export * from "./logger/levels.ts";
+export {
+  Level,
+  levelToName,
+  longestLevelName,
+  nameToLevel,
+} from "./logger/levels.ts";
 export { Logger } from "./logger/logger.ts";
 export { PropertyRedaction } from "./transformers/propertyRedaction.ts";
 export { RegExReplacer } from "./transformers/regExReplacer.ts";
 export { BaseStream } from "./streams/baseStream.ts";
 export { ConsoleStream } from "./streams/consoleStream.ts";
-export * from "./types.ts";
+export {
+  DateTimeFormatter,
+  DateTimeFormatterFn,
+  Filter,
+  FilterFn,
+  Formatter,
+  IllegalStateError,
+  LogMeta,
+  LogRecord,
+  Monitor,
+  MonitorFn,
+  Stream,
+  Transformer,
+  TransformerFn,
+  ValidationError,
+} from "./types.ts";
