ad2daf36bf0797a7cf025f9d9a807f7c3919dfd0
Chris Knight <cknight1234@gmail.com>
2022-02-19 23:03:20 +0000

    chore: upgrade deps

--- deps.ts
+++ deps.ts
@@ -5,4 +5,4 @@ export {
   gray,
   red,
   yellow,
-} from "https://deno.land/std@0.122.0/fmt/colors.ts";
+} from "https://deno.land/std@0.126.0/fmt/colors.ts";
--- test_deps.ts
+++ test_deps.ts
@@ -9,4 +9,4 @@ export {
   assertStrictEquals,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.122.0/testing/asserts.ts";
+} from "https://deno.land/std@0.126.0/testing/asserts.ts";
